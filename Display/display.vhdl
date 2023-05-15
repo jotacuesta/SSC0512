5bed74491c0bb10b677ed35c95dba7da65642611
Jaime Cuesta <jaime.cuestaj@gmail.com>
2023-05-15 13:51:56 -0300

    :bug: Correção do problema com os nomes das saidas

--- Display/display.vhdl
+++ Display/display.vhdl
@@ -15,21 +15,21 @@ architecture behaviour of display is
     signal C : std_logic := entrada(1);
     signal D : std_logic := entrada(0);
 
-    alias a : std_logic is saida(0);
-    alias b : std_logic is saida(1);
-    alias c : std_logic is saida(2);
-    alias d : std_logic is saida(3);
-    alias e : std_logic is saida(4);
-    alias f : std_logic is saida(5);
-    alias g : std_logic is saida(6);
+    alias s_a : std_logic is saida(6);
+    alias s_b : std_logic is saida(5);
+    alias s_c : std_logic is saida(4);
+    alias s_d : std_logic is saida(3);
+    alias s_e : std_logic is saida(2);
+    alias s_f : std_logic is saida(1);
+    alias s_g : std_logic is saida(0);
     
     begin
-        a <= (not(A) and not(B) and not(C) and  (D)) or (not(A) and  (B) and not(C)  and not(D))  or ((A) and not(B) and (C) and not(D))  or ((A) and (B) and not(C) and (D));
-        b <= (not(A) and  (B) and not(C) and  (D)) or (not(A) and  (B) and  (C)  and not(D))  or ((A) and  (C) and (D)) or ((A) and (B) and not(D));
-        c <= (not(A) and not(B) and  (C) and not(D)) or ( (A) and  (B) and  (C))  or ((A) and  (B) and not(D));
-        d <= (not(A) and not(B) and not(C) and  (D)) or (not(A) and (B) and not(C)   and not(D))  or ((B) and (C) and (D))  or ((A) and not(B) and (C) and not(D));
-        e <= (not(A) and (D)) or (not(B) and not(C) and (D)) or (not(A) and (B) and (C));
-        f <= (not(A) and not(B) and (D)) or (not(A) and not(B) and (C)) or (not(A) and (C) and (D)) or ((A) and (B) and not(C) and (D));
-        g <= (not(A) and not(B) and not(C)) or (not(A) and (B) and (C) and (D)) or ((A) and (B) and not(C) and not(D));
+        s_a <= (not(A) and not(B) and not(C) and D) or (not(A) and (B) and not(C) and not(D)) or ((A) and not(B) and (C) and (D))  or ((A) and (B) and not(C) and (D));
+        s_b <= (not(A) and  (B) and not(C) and  (D)) or ((B) and (C)  and not(D))  or ((A) and  (C) and (D)) or ((A) and (B) and not(D));
+        s_c <= (not(A) and not(B) and  (C) and not(D)) or ( (A) and  (B) and  (C))  or ((A) and  (B) and not(D));
+        s_d <= (not(A) and not(B) and not(C) and  (D)) or (not(A) and (B) and not(C) and not(D))  or ((B) and (C) and (D))  or ((A) and not(B) and (C) and not(D));
+        s_e <= (not(A) and (D)) or (not(B) and not(C) and (D)) or (not(A) and (B) and not(C));
+        s_f <= (not(A) and not(B) and (D)) or (not(A) and not(B) and (C)) or (not(A) and (C) and (D)) or ((A) and (B) and not(C) and (D));
+        s_g <= (not(A) and not(B) and not(C)) or (not(A) and (B) and (C) and (D)) or ((A) and (B) and not(C) and not(D));
 
 end behaviour;
